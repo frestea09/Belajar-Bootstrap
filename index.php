181113232858d73144fee145dbc16da98e446287
ilman teguh prasetya <ilmanfrasetya@gmail.com>
2019-04-13 20:36:47 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>
+            Belajad BootsTrap
+        </title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">    </head>
+    <body>
+<!--        <div class="container">-->
+<!--            <div class="nav">-->
+<!--
+<!--            </div>-->
+<!--            <div class="main">-->
+<!--                <h1 class="page-header">-->
+<!--                    Welcome to Home <small>Created by ilman</small>-->
+<!--                </h1>-->
+<!--                <p>-->
+<!--                    Selamat Datang, website ini dibuat menggunakan Bootstrap 4, dan dirancang oleh <mark> ilman teguh prasetya</mark>-->
+<!--                    <del>Haram</del> dan <u>Halal</u>-->
+<!--                </p>-->
+<!--                <hr>-->
+<!--                <p class="text-left text-lowercase">lef align text</p>-->
+<!--                <p class="text-right text-uppercase">right align text</p>-->
+<!--                <p class="text-center text-capitalize">Center algin text</p>-->
+<!--                <p class="text-justify">justified text</p>-->
+<!--                <p class="text-nowrap">text no wrap text</p>-->
+<!--                <hr>-->
+<!--                <div class="float-left">Float left on all viewport sizes</div><br>-->
+<!--                <div class="float-right">Float right on all viewport sizes</div><br>-->
+<!--                <div class="clearfix">Don't float on all viewport sizes</div>-->
+<!--                <hr>-->
+<!--                <blockquote class="blockquote">-->
+<!--                    <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>-->
+<!--                </blockquote>-->
+<!--                <hr>-->
+<!--<!--                list-->
+<!--                <ul class="list-unstyled">-->
+<!--                    <li class="list-inline-item">Ganteng</li>-->
+<!--                    <li class="list-inline-item">Ganteng</li>-->
+<!--                    <li class="list-inline-item">Ganteng</li>-->
+<!--                    <li class="list-inline-item">Ganteng</li>-->
+<!--                    <li class="list-inline-item">Ganteng</li>-->
+<!--                </ul>-->
+<!--                <hr>-->
+<!--                <ul class="list-inline">-->
+<!--                    <li class="list-inline-item">Lorem ipsum</li>-->
+<!--                    <li class="list-inline-item">Phasellus iaculis</li>-->
+<!--                    <li class="list-inline-item">Nulla volutpat</li>-->
+<!--                </ul>-->
+<!--                <hr>-->
+<!--<!--                code-->
+<!--                <pre><code>&lt;p&gt;Sample text here...&lt;/p&gt;-->
+<!--                &lt;p&gt;And another line of sample text here...&lt;/p&gt;-->
+<!--                </code></pre>-->
+<!--                <br>-->
+<!--                Change directory with <kbd>cd</kbd>-->
+<!--                to do setting, pres <kbd><kbd>ctrl</kbd>+<kbd>,</kbd></kbd>-->
+<!--                <p class="text-primary">Sukses</p>-->
+<!--                <button class="btn btn-danger">Bahaya</button>-->
+<!--<!--                contextual color-->
+<!--                <p class="text-primary">Hello World</p>-->
+<!--                <p class="text-success">Hello World</p>-->
+<!--                <p class="text-danger">Hello World</p>-->
+<!--                <p class="text-info">Hello World</p>-->
+<!--                <p class="text-danger">Hello World</p>-->
+<!--                <p class="text-dark">Hello World</p>-->
+<!--                <p class="text-muted">Hello World</p>-->
+<!--
+<!--             backgroudn-->
+<!--                <p class="bg-info">Hello World</p>-->
+<!--                <p class="bg-danger">Hello World</p>-->
+<!--                <p class="bg-secondary">Hello World</p>-->
+<!--                <p class="bg-primary">Hello World</p>-->
+<!--                <p class="bg-warning">Hello World</p>-->
+<!--                <p class="bg-mute">Hello World</p>-->
+<!--            </div>-->
+
+
+<!--        </div>-->
+<!--        <div class="container">-->
+<!--            <button class="btn btn-danger">Default</button>-->
+<!--            <input name="btnSubmit" type="button" class="btn btn-success" value="Submit"/>-->
+<!--            <button class="btn btn-default btn-lg">Default</button>-->
+<!--            <button class="btn btn-primary btn-sm">Default</button>-->
+<!--            <button class="btn btn-primary btn-xs" disabled="disabled">Default</button>-->
+<!--        </div>-->
+<!--            <div class="container">-->
+<!--                <div class="form-group">-->
+<!--                    <label>Name</label>-->
+<!--                    <input type="text" class="form-control" placeholder="Add Name">-->
+<!--                </div>-->
+<!--                <div class="form-group">-->
+<!--                    <label>Email</label>-->
+<!--                    <input type="text" class="form-control" placeholder="Add Email">-->
+<!--                </div>-->
+<!--                <div class="form-group">-->
+<!--                    <label>Message</label>-->
+<!--                    <textarea class="form-control" placeholder="add Message"></textarea>-->
+<!--                </div>-->
+<!--                <div class="form-group">-->
+<!--                    <label>Gender</label>-->
+<!--                    <select class="form-control" name="insertGender">-->
+<!--                        <option value="female">Female</option>-->
+<!--                        <option value="male">Male</option>-->
+<!--                    </select>-->
+<!--                </div>-->
+<!--                <div class="form-group">-->
+<!--                    <label>Upload File</label>-->
+<!--                    <input type="file">-->
+<!--                    <p class="help-block">only png and jpg allowed</p>-->
+<!--                </div>-->
+<!--                <div class="form-group">-->
+<!--                    <label>Hobby</label>-->
+<!--                    <div class="clearfix"></div>-->
+<!--                    <div class="form-check form-check-inline">-->
+<!--                        <input type="checkbox" name="hobby[]" class="form-check-input" id="inLineCheckBox2">-->
+<!--                        <label class="form-check-label" for="inLineCheckBox2">Coding</label>-->
+<!--                    </div>-->
+<!--                    <div class="form-check form-check-inline">-->
+<!--                        <input type="checkbox" class="form-check-input" id="inLineCheckBox1" name="hobby[]">-->
+<!--                        <label class="form-check-label" for="inLineCheckBox1">Baca</label>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--            <div class="container">-->
+<!--                <form class="form-group">-->
+<!--                    <table class="table">-->
+<!--                        <thead>-->
+<!--                        <tr>-->
+<!--                            <th scope="col">#</th>-->
+<!--                            <th scope="col">head satu</th>-->
+<!--                            <th scope="col">head dua</th>-->
+<!--                            <th scope="col">head tiga</th>-->
+<!--                            <th scope="col">head empat</th>-->
+<!--                        </tr>-->
+<!--                        </thead>-->
+<!--                        <tbody>-->
+<!--                        <tr>-->
+<!--                            <th scope="row">1</th>-->
+<!--                            <td>ganteng</td>-->
+<!--                            <td>ganteng</td>-->
+<!--                            <td>ganteng</td>-->
+<!--                            <td>ganteng</td>-->
+<!--                        </tr>-->
+<!--                        <tr>-->
+<!--                            <th scope="row">2</th>-->
+<!--                            <td>ganteng</td>-->
+<!--                            <td>ganteng</td>-->
+<!--                            <td>ganteng</td>-->
+<!--                            <td>ganteng</td>-->
+<!--                        </tr>-->
+<!--                        <tr>-->
+<!--                            <th scope="row">3</th>-->
+<!--                            <td>ganteng</td>-->
+<!--                            <td>ganteng</td>-->
+<!--                            <td>ganteng</td>-->
+<!--                            <td>ganteng</td>-->
+<!--                        </tr>-->
+<!--                        </tbody>-->
+<!--                    </table>-->
+<!--                    <button type="submit" class="btn btn-primary">Save</button>-->
+<!--                </form>-->
+<!--            </div>-->
+<!--                <div class="container">-->
+<!--                    <form class="form-inline">-->
+<!--                        <div class="form-group">-->
+<!--                            <label>Username</label>-->
+<!--                            <input type="email" id="insertUsername" name="insertUserName" class="form-control" placeholder="Username">-->
+<!--                        </div>-->
+<!--                        <div class="form-group">-->
+<!--                            <label>Password</label>-->
+<!--                            <input type="password" id="insertPassword" name="insertPassword" class="form-control">-->
+<!--                        </div>-->
+<!--                        <div class="form-check">-->
+<!--                            <label>-->
+<!--                                <input type="checkbox"> Remember me-->
+<!--                            </label>-->
+<!--                        </div>-->
+<!--                        <button class="btn btn-primary">Login</button>-->
+<!--                    </form>-->
+<!--                </div>-->
+<!--                    <div class="container">-->
+<!--                        <table class="table table-bordered table-hover table-striped table-condensed">-->
+<!--                            <tr>-->
+<!--                                <th>No</th>-->
+<!--                                <th>First Name</th>-->
+<!--                                <th>Last Name</th>-->
+<!--                                <th>Mobile Number</th>-->
+<!--                            </tr>-->
+<!--                            <tr class="table-primary">-->
+<!--                                <td>1</td>-->
+<!--                                <td>Ilman</td>-->
+<!--                                <td>Prasetya</td>-->
+<!--                                <td>081931472811</td>-->
+<!--                            </tr>-->
+<!--                            <tr>-->
+<!--                                <td>1</td>-->
+<!--                                <td>Ilman</td>-->
+<!--                                <td>Prasetya</td>-->
+<!--                                <td>081931472811</td>-->
+<!--                            </tr>-->
+<!--                            <tr class="table-active">-->
+<!--                                <td>1</td>-->
+<!--                                <td>Ilman</td>-->
+<!--                                <td>Prasetya</td>-->
+<!--                                <td>081931472811</td>-->
+<!--                            </tr>-->
+<!--                            <tr>-->
+<!--                                <td>1</td>-->
+<!--                                <td>Ilman</td>-->
+<!--                                <td>Prasetya</td>-->
+<!--                                <td>081931472811</td>-->
+<!--                            </tr>-->
+<!--                        </table>-->
+<!--                    </div>-->
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+</html>
